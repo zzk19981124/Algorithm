24c120a258cdd738aaad3c7e035a8238c327355f
icegps_cky <chenkaiyang@icegps.com.cn>
2021-07-26 17:36:24 +0800

    commit1

--- app/src/main/java/com/example/algorithm/utils/WriteFileUil.java
+++ app/src/main/java/com/example/algorithm/utils/WriteFileUil.java
@@ -42,6 +42,7 @@ public class WriteFileUil {
         try {
             file = new File(filepath+filename);
             if (!file.exists()){
+                //这儿报错
                 file.createNewFile();
             }
         } catch (Exception exception) {
